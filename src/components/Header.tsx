65a445d0f961487fdf513e0237b6f725960298d7
Rodolfo <rodolfotiago@gmail.com>
2025-07-02 15:39:59 -0300

    feat: mongo v1 (localmente) - dockerfile prod

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ const Header: React.FC = () => {
           <div className="text-center">
             <h1 className="text-xl font-bold">💰 Controle Financeiro</h1>
             <p className="text-blue-100 text-sm mt-1">
-              Rodolfo & Thaís | v{appVersion} 🚀
+              _______________ | v{appVersion} 🚀
             </p>
           </div>
           <div
